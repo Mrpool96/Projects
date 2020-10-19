06050dd84497e718ee065e7fe4b0fe2575b5da15
Sagar Karambelkar <53513296+Mrpool96@users.noreply.github.com>
2020-10-19 13:28:57 +0530

    I have written a simple chat server that can serve multiple clients using Socket programming and multithreading in Java.
    I have written a simple chat server that can serve multiple clients using Socket programming and multithreading in Java. Each client can send message to server, which then broadcasts the message to all other clients currently connected.
    
    Each client is identified by a name, which is taken from user at the time of starting the client.
    Chat Server

---
+++ ChatServer.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Stack;
+import java.util.Vector;
+
+public class ChatServer {
+    Vector<String> users = new Vector<String>();
+    Vector<HandleClient> clients =new Vector<HandleClient>();
+    public void process () throws Exception{
+        ServerSocket server = new ServerSocket(9999,10);
+        System.out.println("Server Started...");
+        while (true){
+            Socket client = server.accept();
+            HandleClient c = new HandleClient (client);
+            clients.add(c);
+
+        }
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        new ChatServer().process();
+
+    }
+        public void broadcast(String user, String message){
+        //send message to all connected user
+            for(HandleClient c : clients)
+                if (! c.getUserName().equals(user))
+                    c.sendMessage(user,message);
+
+        }
+  class HandleClient extends Thread{
+
+        String name = " ";
+        BufferedReader input;
+        PrintWriter output;
+        public HandleClient(Socket client) throws  Exception{
+            //get input and output streams
+            input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            output = new PrintWriter(client.getOutputStream());
+            //read
+
+            name = input.readLine();
+            users.add(name);// add to vector
+            start();
+
+        }
+        public void  sendMessage(String uname, String msg){
+            System.out.println(uname+ " : " +msg);
+        }
+        public String getUserName(){
+            return name;
+        }
+        public void run(){
+            String line;
+            try {
+                while (true){
+                    line = input.readLine();
+                    if (line.equals("end")){
+                        clients.remove(this);
+                        users.remove(name);
+                        break;
+                    }
+                    broadcast(name,line);
+                }
+            }
+            catch (Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+
+  }
+}
